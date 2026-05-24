4c5dff1ba401cebfc5b6aeb612782a7c440740fc
miwanoff <marina.iva.noffa@gmail.com>
2026-05-24 13:11:47 +0300

    Add path

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
 const http = require('http');
+const path = require('path');
 const port = 3060;
 
 
 const server = http.createServer(app);
 
 
-app.get('/healthcheck', (req, res) => {
+// app.get('/', (req, res) => {
+//     res.send('<h1>Start...</h1>');
+// });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+
+app.get('/test', (req, res) => {
     res.send('<h1>RPS App running...</h1>');
 });
 
+app.use(express.static(path.join(__dirname, 'client')));
 
 server.listen(port, () => {
     console.log(`listening on *:${port}`);
