1277c9ac98f403dbede26ccaed138b25ef862593
miwanoff <marina.iva.noffa@gmail.com>
2026-05-31 13:18:04 +0300

    Add declareWinner

--- client/client.js
+++ client/client.js
@@ -11,6 +11,7 @@ function сreateGame() {
 }
 
 function joinGame() {
+  // player1 = false;
   roomUniqueId = document.getElementById("roomUniqueId").value;
   socket.emit("joinGame", { roomUniqueId: roomUniqueId });
   console.log("joinGame " + roomUniqueId);
@@ -18,10 +19,13 @@ function joinGame() {
 
 function sendChoice(rpsValue) {
   const choiceEvent = player1 ? "p1Choice" : "p2Choice";
+  console.log(choiceEvent);
+  // Emit the event of the current player
   socket.emit(choiceEvent, {
-    rpsValue: rpsValue,
-    roomUniqueId: roomUniqueId,
+    rpsValue: rpsValue, // the choice
+    roomUniqueId: roomUniqueId, // room
   });
+  // Show the choice of the current player
   let playerChoiceButton = document.createElement("button");
   playerChoiceButton.style.display = "block";
   playerChoiceButton.classList.add(rpsValue.toString().toLowerCase());
@@ -29,6 +33,17 @@ function sendChoice(rpsValue) {
   document.getElementById("player1Choice").innerHTML = "";
   document.getElementById("player1Choice").appendChild(playerChoiceButton);
 }
+
+function createOpponentChoiceButton(data) {
+    document.getElementById('opponentState').innerHTML = "Opponent made a choice";
+    let opponentButton = document.createElement('button');
+    opponentButton.id = 'opponentButton';
+    opponentButton.classList.add(data.rpsValue.toString().toLowerCase());
+    opponentButton.style.display = 'none';
+    opponentButton.innerText = data.rpsValue;
+    document.getElementById('player2Choice').appendChild(opponentButton);
+}
+
 
 socket.on("newGame", (data) => {
   roomUniqueId = data.roomUniqueId;
@@ -58,4 +73,36 @@ socket.on("playersConnected", (data) => {
   document.getElementById("initial").style.display = "none";
   document.getElementById("waitingArea").style.display = "none";
   document.getElementById("gameArea").style.display = "block";
+});
+
+socket.on("p1Choice",(data)=>{
+    if(!player1) {
+        createOpponentChoiceButton(data);
+    }
+});
+
+socket.on("p2Choice",(data)=>{
+    if(player1) {
+        createOpponentChoiceButton(data);
+    }
+});
+
+socket.on("result",(data)=>{
+    let winnerText = '';
+    if(data.winner != 'd') {
+        if(data.winner == 'p1' && player1) {
+            winnerText = 'You win';
+        } else if(data.winner == 'p1') {
+            winnerText = 'You lose';
+        } else if(data.winner == 'p2' && !player1) {
+            winnerText = 'You win';
+        } else if(data.winner == 'p2') {
+            winnerText = 'You lose';
+        }
+    } else {
+        winnerText = `It's a draw`;
+    }
+    document.getElementById('opponentState').style.display = 'none';
+    document.getElementById('opponentButton').style.display = 'block';
+    document.getElementById('winnerArea').innerHTML = winnerText;
 });
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const path = require("path");
 const port = 3060;
 
 const { Server } = require("socket.io");
@@ -12,26 +12,24 @@ const io = new Server(server);
 
 const rooms = {};
 
-
 // app.get('/', (req, res) => {
 //     res.send('<h1>Start...</h1>');
 // });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
 });
 
-
-app.get('/test', (req, res) => {
-    res.send('<h1>RPS App running...</h1>');
+app.get("/test", (req, res) => {
+  res.send("<h1>RPS App running...</h1>");
 });
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, "client")));
 
 io.on("connection", (socket) => {
-  console.log(`a user ${socket.id} is connected`); 
+  console.log(`a user ${socket.id} is connected`);
 
-   socket.on("createGame", () => {
+  socket.on("createGame", () => {
     console.log("createGame");
     const roomUniqueId = makeid(12);
     console.log(roomUniqueId);
@@ -46,21 +44,36 @@ io.on("connection", (socket) => {
       socket.join(data.roomUniqueId);
       socket.to(data.roomUniqueId).emit("playersConnected", {});
       socket.emit("playersConnected");
-      console.log("playersConnected " + data.roomUniqueId)
+      console.log("playersConnected " + data.roomUniqueId);
+    }
+  });
+
+  socket.on("p1Choice", (data) => {
+    let rpsValue = data.rpsValue;
+    rooms[data.roomUniqueId].p1Choice = rpsValue;
+    socket.to(data.roomUniqueId).emit("p1Choice", { rpsValue: data.rpsValue });
+    if (rooms[data.roomUniqueId].p2Choice != null) {
+      declareWinner(data.roomUniqueId);
+    }
+  });
+
+  socket.on("p2Choice", (data) => {
+    let rpsValue = data.rpsValue;
+    rooms[data.roomUniqueId].p2Choice = rpsValue;
+    socket.to(data.roomUniqueId).emit("p2Choice", { rpsValue: data.rpsValue });
+    if (rooms[data.roomUniqueId].p1Choice != null) {
+      declareWinner(data.roomUniqueId);
     }
   });
 
   socket.on("disconnect", () => {
     console.log(`user ${socket.id} is disconnected`);
   });
-
 });
-
 
 server.listen(port, () => {
-    console.log(`listening on *:${port}`);
+  console.log(`listening on *:${port}`);
 });
-
 
 function makeid(length) {
   var result = "";
@@ -71,4 +84,36 @@ function makeid(length) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
+}
+
+function declareWinner(roomUniqueId) {
+    let p1Choice = rooms[roomUniqueId].p1Choice;
+    let p2Choice = rooms[roomUniqueId].p2Choice;
+    let winner = null;
+    if (p1Choice === p2Choice) {
+        winner = "d";
+    } else if (p1Choice == "Paper") {
+        if (p2Choice == "Scissor") {
+            winner = "p2";
+        } else {
+            winner = "p1";
+        }
+    } else if (p1Choice == "Rock") {
+        if (p2Choice == "Paper") {
+            winner = "p2";
+        } else {
+            winner = "p1";
+        }
+    } else if (p1Choice == "Scissor") {
+        if (p2Choice == "Rock") {
+            winner = "p2";
+        } else {
+            winner = "p1";
+        }
+    }
+    io.sockets.to(roomUniqueId).emit("result", {
+        winner: winner
+    });
+    rooms[roomUniqueId].p1Choice = null;
+    rooms[roomUniqueId].p2Choice = null;
 }
